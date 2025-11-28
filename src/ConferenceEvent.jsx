1135e54702baf910c619f9e52feedd690de8a0a5
Kyle <kylemoored90@gmail.com>
2025-11-28 16:29:27 -0500

    page 7 complete 3rd correction, meal slice import name

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -4,7 +4,7 @@ import TotalCost from "./TotalCost";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
 import { incrementAvQuantity, decrementAvQuantity} from "./avSlice";
-import { toggleMealSelection } from "./MealsSlice"
+import { toggleMealSelection } from "./mealsSlice"
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
